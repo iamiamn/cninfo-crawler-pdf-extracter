2ad0989f2a53459809ece89d88ad2cc6cebab664
iamiman <iamiman94@hotmail.com>
2018-09-21 09:37:07 -0500

    Update htmlPretty.py

--- txtAndXls/htmlPretty.py
+++ txtAndXls/htmlPretty.py
@@ -1,4 +1,7 @@
+#Here I use BeautifulSoup to extract Html content
 from bs4 import BeautifulSoup
+
+
 fhand = open('merge0_.html', 'r', encoding = 'utf-8')
 content = fhand.read()#因为encoding 是utf-8,这里read到的是utf-8编码的字符串
 fhand.close()
